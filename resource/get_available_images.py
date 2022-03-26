82ebe3b5033b0de32d1651b747b40a28588e8e16
admin <admin>
2022-03-26 16:44:07 -0400

    Edited file 'resource/get_available_images.py'

--- resource/get_available_images.py
+++ resource/get_available_images.py
@@ -4,6 +4,7 @@ import os
 def do(payload, config, plugin_config, inputs):
   data_dir=dataiku.get_custom_variables()["dip.home"]
   img_path=os.path.join(data_dir,'/local/static/images/webapps')
+  print("alex test1 "+str(data_dir))
   img_list=os.listdir(img_path)
   print(str(img_list))
   choices={}
