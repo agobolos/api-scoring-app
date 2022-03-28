0e9c8e5b8e912637aeffc86d7fe9d8a2a8336ae6
admin <admin>
2022-03-28 10:24:12 -0400

    Edited file 'webapps/scoring-app/backend.py'

--- webapps/scoring-app/backend.py
+++ webapps/scoring-app/backend.py
@@ -119,7 +119,7 @@ def generate_input(key):
 
 
 app.layout = html.Div([
-        dbc.Row(html.Div(html.Img(style={'height':'100px'}, src=header_img)), style=pic_style),
+        dbc.Row(html.Div(html.Img(style={'height':'100px'}, src=header_img), style=pic_style)),
         html.Br(),
     
         dbc.Row([
